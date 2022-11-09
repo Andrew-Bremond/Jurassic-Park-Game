2bc1f248289e4fe635513a574f84c6922168d457
Kelsey Musolf <kelseymusolf@ucr-secure-29-10-13-166-241.wnet.ucr.edu>
2022-11-08 17:48:25 -0800

    basics of story and ellie and character

--- header/Character.h
+++ header/Character.h
@@ -1,1 +1,27 @@
 //character.h file
+
+#include "header/Ellie.h"
+#include "header/AlanKids.h"
+#include "header/Nedry.h"
+#include "header/IanHammond.h"
+#include "header/Story.h"
+
+
+#include <string>
+
+using namespace std;
+
+#ifndef __CHARACTER_H__
+#define __CHARACTER_H__
+
+
+class Character : public Story{
+public:
+    void dies();
+    void lives();
+    void getInfo();
+
+
+};
+
+#endif
--- header/Ellie.h
+++ header/Ellie.h
@@ -1,1 +1,21 @@
 //Ellie.h
+
+#include <string>
+#include "header/Character.h"
+
+using namespace std;
+
+#ifndef __ELLIE_H__
+#define __ELLIE_H__
+
+class Ellie: public Character{
+ public:
+    
+    void sceneOne ();
+    void sceneTwo ();
+    void sceneThree ();
+    void sceneFour ();
+
+};
+
+#endif
