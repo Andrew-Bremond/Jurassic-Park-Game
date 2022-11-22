65fc739d2d3432b6ebd2b580325c527a77360bbb
aasthaamin <aasuamin@gmail.com>
2022-11-21 21:15:17 -0800

    Nedry header file and scene 1

--- header/Nedry.h
+++ header/Nedry.h
@@ -1,1 +1,21 @@
-//Nedry.h
+#include <iostream>
+#include <string>
+#include "Character.h"
+
+using namespace std;
+
+#ifndef __NEDRY_H__
+#define __NEDRY_H__
+
+class Nedry: public Character{
+ public:
+    Nedry();
+    void setup();
+    void sceneOne ();
+    void sceneTwo ();
+    void sceneThree ();
+    void sceneFour ();
+
+};
+
+#endif
