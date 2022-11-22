3f744bc63f38134ab5cd397097f1f2230a6ffdf7
aasthaamin <aasuamin@gmail.com>
2022-11-21 20:56:03 -0800

    AlanKids header file and scene 4

--- header/AlanKids.h
+++ header/AlanKids.h
@@ -1,1 +1,22 @@
 //Alan and the kids header file
+#include <iostream>
+#include <string>
+#include "Character.h"
+
+using namespace std;
+
+#ifndef __ALANKIDS_H__
+#define __ALANKIDS_H__
+
+class AlanKids: public Character{
+ public:
+    AlanKids();
+    void setup();
+    void sceneOne ();
+    void sceneTwo ();
+    void sceneThree ();
+    void sceneFour ();
+
+};
+
+#endif
