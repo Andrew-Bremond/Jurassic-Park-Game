15c811072e78b53f48e6b6d199850df9b5dcbb94
Andrew-Bremond <78613135+Andrew-Bremond@users.noreply.github.com>
2022-11-26 18:47:32 -0800

    Update Character.cpp

--- src/Character.cpp
+++ src/Character.cpp
@@ -7,13 +7,18 @@ using namespace std;
 void Character::dies() {
     string choice;
     cout << "You've lost the game" << endl;
-    cout << "Would you like to start over 'r' or quit the game 'q'?" << endl;
+    cout << "Press 'r' to start over " << endl;
+    cout << "or " << endl;
+    cout << "Press any character to quit the game" << endl;
+    cout << endl;
     cin >> choice;
+    cout << endl;
 
     if (choice == "r") {
         intro();
     }
-    else if (choice == "q") {
+    else {
+        cout << "Exiting Game..." << endl;
         exit(0);
     }
 }
@@ -22,13 +27,18 @@ void Character::lives() {
     string choice;
     cout << "\n----------------------------------\n";
     cout << "Congrats you've survived Jurassic Park! Your character is on a helicopter to safety, off the island, and you've finished the game!" << endl;
-    cout << "Would you like to play again 'r' or quit the game 'q'?" << endl;
+    cout << "Press 'r' to start over " << endl;
+    cout << "or " << endl;
+    cout << "Press any character to quit the game" << endl;
+    cout << endl;
     cin >> choice;
+    cout << endl;
 
     if (choice == "r") {
         intro();
     }
-    else if (choice == "q") {
+    else {
+        cout << "Exiting Game..." << endl;
         exit(0);
     }
 }
